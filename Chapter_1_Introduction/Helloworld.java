f473f3d927da1e8b103160d0dfa693fff4b37724
srunjani <srunjani@gmail.com>
2024-10-15 19:37:52 -0500

    completed task 1 in lab1

---
+++ Chapter_1_Introduction/Helloworld.java
@@ -0,0 +1,10 @@
+class Helloworld {
+    public static void main(String args[]) {
+    System.out.println("first name:" +args[0]);
+    System.out.println("second name:" +args[1]);
+    int age = Integer.parseInt(args[2]);
+    System.out.println("age:" +age);
+    }
+        
+    
+    }
