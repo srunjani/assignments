5fe2a19b2059bbb54576385af0b3e06ffd02ce52
srunjani <srunjani@gmail.com>
2024-10-21 19:36:54 -0500

    done with lab 1 first program don't ask me tdo it again.

--- Chapter_1_Introduction/Helloworld.java
+++ Chapter_1_Introduction/Helloworld.java
@@ -1,9 +1,22 @@
 class Helloworld {
     public static void main(String args[]) {
-        int age=0;
-        System.out.println("first name:" + args[0]);
-        System.out.println("second name:" + args[1]);
-        age = Integer.parseInt(args[2]);
-        System.out.println("age:" + age);
+        if(args.length==3){
+            System.out.println("please enter the values");
+            System.out.println("first name:"+args[0]);
+            System.out.println("second name:"+args[1]);
+            try{
+                int age = Integer.parseInt(args[2]);
+                System.out.println("age:"+age);
+            }
+            catch(Exception e){
+                System.out.println("please enter valid age");
+            }
+        }
+        else{
+            System.out.println("please enter three command line arguments");
+        }
+
+
+
     }
 }
