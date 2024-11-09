d6f62a045a6d6978ceab476753b8c21d1d904007
srunjani <srunjani@gmail.com>
2024-11-08 20:46:48 -0600

    DONE WITH MY LAB2 in lab1, DON'T ASK ME TO DO IT AGAIN

---
+++ Chapter_1_Introduction/Song.java
@@ -0,0 +1,30 @@
+public class Song
+{
+    public static void main(String[] args){
+
+       secondLine();
+       gap();
+       secondLine();
+       thirdLine();
+       secondLine();
+       gap();
+       secondLine();
+       thirdLine();
+       secondLine();
+       gap();
+       secondLine();
+
+    }
+    public static void secondLine(){
+        System.out.println("Go, team, go!\n"+
+                "You can do it.");
+    }
+    public static void thirdLine(){
+        System.out.println( "You're the best,\n"+"In the West." );
+    }
+    public static void gap(){
+        System.out.println("\n");
+    }
+
+
+}
